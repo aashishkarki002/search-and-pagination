4edd5cfa6b9440c89816ffb16b0d027aa6d6d3b7
aashishkarki002 <karkiaashish6002@gmail.com>
2025-05-22 11:10:33 +0545

    search feature created

---
+++ components/Input.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+
+export default function Input() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="peer block w-72 rounded-md border 
+          border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+    </div>
+  );
+}
